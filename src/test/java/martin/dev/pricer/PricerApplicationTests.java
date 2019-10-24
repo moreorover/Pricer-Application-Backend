cab9089e65b51f1179c7bfc134b233dc9f7e822d
Martin <mselvenis@gmail.com>
2019-10-24 22:37:09 +0100

    init

---
+++ src/test/java/martin/dev/pricer/PricerApplicationTests.java
@@ -0,0 +1,13 @@
+package martin.dev.pricer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
