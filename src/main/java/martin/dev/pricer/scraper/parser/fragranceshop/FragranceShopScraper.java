ce07dd578c9930f04932e3f532f7de8ff0125a99
Martin <mselvenis@gmail.com>
2019-11-01 22:54:58 +0000

    fragrance shop files created

---
+++ src/main/java/martin/dev/pricer/scraper/parser/fragranceshop/FragranceShopScraper.java
@@ -0,0 +1,25 @@
+package martin.dev.pricer.scraper.parser.fragranceshop;
+
+import lombok.extern.slf4j.Slf4j;
+import martin.dev.pricer.data.model.store.StoreUrl;
+import martin.dev.pricer.scraper.parser.Scraper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FragranceShopScraper extends Scraper {
+
+    @Autowired
+    private FragranceShopParser fragranceShopParser;
+
+    @Override
+    public void scrapePages(StoreUrl storeUrl) {
+
+    }
+
+    @Override
+    public String makeNextPageUrl(int pageNum) {
+        return null;
+    }
+}
