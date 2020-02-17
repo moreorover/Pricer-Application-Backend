87f0e02197452bdd51342e9d1326ee254a089e35
Martin <mselvenis@gmail.com>
2020-02-17 13:38:14 +0000

    cleaning models

--- src/main/java/martin/dev/pricer/data/model/mongo/model/Price.java
+++ src/main/java/martin/dev/pricer/data/model/mongo/model/Price.java
@@ -1,52 +1,15 @@
 package martin.dev.pricer.data.model.mongo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
 public class Price {
 
     private Double price;
     private Double delta;
     private LocalDateTime foundAt;
-
-    public Price() {
-    }
-
-    public Price(Double price, Double delta, LocalDateTime foundAt) {
-        this.price = price;
-        this.delta = delta;
-        this.foundAt = foundAt;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Double getDelta() {
-        return delta;
-    }
-
-    public void setDelta(Double delta) {
-        this.delta = delta;
-    }
-
-    public LocalDateTime getFoundAt() {
-        return foundAt;
-    }
-
-    public void setFoundAt(LocalDateTime foundAt) {
-        this.foundAt = foundAt;
-    }
-
-    @Override
-    public String toString() {
-        return "Price{" +
-                "price=" + price +
-                ", delta=" + delta +
-                ", foundAt=" + foundAt +
-                '}';
-    }
 }
