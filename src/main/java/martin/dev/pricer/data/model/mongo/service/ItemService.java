fd10eff49aa58d4e14b17299aed2ba69c31f5046
Martin <mselvenis@gmail.com>
2020-02-21 15:56:40 +0000

    mongo services

--- src/main/java/martin/dev/pricer/data/model/mongo/service/ItemService.java
+++ src/main/java/martin/dev/pricer/data/model/mongo/service/ItemService.java
@@ -1,9 +1,21 @@
 package martin.dev.pricer.data.model.mongo.service;
 
 import martin.dev.pricer.data.model.mongo.model.Item;
+import martin.dev.pricer.data.model.mongo.model.Price;
+import martin.dev.pricer.data.model.mongo.model.Store;
+import martin.dev.pricer.data.model.mongo.model.Url;
+import martin.dev.pricer.scraper.model.ParsedItemDto;
 
 public interface ItemService {
     Item findByUpc(String upc);
 
     Item save(Item newItem);
+
+    Item buildItemOfParsedData(ParsedItemDto parsedItemDto, Store store, Url url);
+
+    double calculateDelta(double oldPrice, double newPrice);
+
+    Item newPrice(Item item, double newPrice);
+
+    void processParsedItem(ParsedItemDto parsedItemDto, Store store, Url url);
 }
--- src/main/java/martin/dev/pricer/data/model/mongo/service/MongoItemService.java
+++ src/main/java/martin/dev/pricer/data/model/mongo/service/MongoItemService.java
@@ -2,9 +2,16 @@ package martin.dev.pricer.data.model.mongo.service;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import martin.dev.pricer.data.model.mongo.model.Item;
+import martin.dev.pricer.data.model.mongo.model.*;
+import martin.dev.pricer.data.model.mongo.repository.MongoDealRepository;
 import martin.dev.pricer.data.model.mongo.repository.MongoItemRepository;
+import martin.dev.pricer.scraper.model.ParsedItemDto;
 import org.springframework.stereotype.Service;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Slf4j
 @Service
@@ -12,9 +19,11 @@ import org.springframework.stereotype.Service;
 public class MongoItemService implements ItemService {
 
     private MongoItemRepository itemRepository;
+    private MongoDealRepository dealRepository;
 
-    public MongoItemService(MongoItemRepository itemRepository) {
+    public MongoItemService(MongoItemRepository itemRepository, MongoDealRepository dealRepository) {
         this.itemRepository = itemRepository;
+        this.dealRepository = dealRepository;
     }
 
     @Override
@@ -25,5 +34,58 @@ public class MongoItemService implements ItemService {
     @Override
     public Item save(Item newItem) {
         return this.itemRepository.save(newItem);
+    }
+
+    @Override
+    public Item buildItemOfParsedData(ParsedItemDto parsedItemDto, Store store, Url url) {
+        Set<Price> prices = new HashSet<>();
+        prices.add(new Price(parsedItemDto.getPrice(),
+                0.0,
+                LocalDateTime.now()));
+
+        return new Item(parsedItemDto.getUpc(),
+                parsedItemDto.getTitle(),
+                parsedItemDto.getUrl(),
+                parsedItemDto.getImg(),
+                prices,
+                url.getCategories(),
+                store,
+                parsedItemDto.getUrlFound());
+    }
+
+    @Override
+    public double calculateDelta(double oldPrice, double newPrice) {
+        double delta = 100 * ((newPrice - oldPrice) / oldPrice);
+        DecimalFormat df = new DecimalFormat("#.##");
+        return Double.parseDouble(df.format(delta));
+    }
+
+    @Override
+    public Item newPrice(Item item, double newPrice) {
+        double delta = calculateDelta(item.getLastPrice(), newPrice);
+        Price newPriceObj = new Price(newPrice, delta, LocalDateTime.now());
+        item.getPrices().add(newPriceObj);
+        return item;
+    }
+
+    @Override
+    public void processParsedItem(ParsedItemDto parsedItemDto, Store store, Url url) {
+        if (parsedItemDto.isValid()) {
+
+            Item dbItem = findByUpc(parsedItemDto.getUpc());
+
+            if (dbItem == null) {
+                Item buildItem = buildItemOfParsedData(parsedItemDto, store, url);
+                save(buildItem);
+            } else if (dbItem.getLastPrice() != parsedItemDto.getPrice() && dbItem.getLastPrice() > parsedItemDto.getPrice()) {
+                newPrice(dbItem, parsedItemDto.getPrice());
+                save(dbItem);
+                Deal newDeal = new Deal(dbItem, dbItem.getCategories(), store, LocalDateTime.now(),true);
+                dealRepository.save(newDeal);
+            } else if (dbItem.getLastPrice() != parsedItemDto.getPrice()) {
+                newPrice(dbItem, parsedItemDto.getPrice());
+                save(dbItem);
+            }
+        }
     }
 }
