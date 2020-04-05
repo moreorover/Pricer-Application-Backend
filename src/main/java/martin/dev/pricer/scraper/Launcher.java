89c09edd1af30f038caac805a8851fac9ffbdaca
moreorover <mselvenis@gmail.com>
2020-04-05 21:08:03 +0100

    method to save stores into db

--- src/main/java/martin/dev/pricer/scraper/Launcher.java
+++ src/main/java/martin/dev/pricer/scraper/Launcher.java
@@ -1,9 +1,18 @@
 package martin.dev.pricer.scraper;
 
+import martin.dev.pricer.data.model.Category;
 import martin.dev.pricer.data.model.Status;
+import martin.dev.pricer.data.model.Store;
 import martin.dev.pricer.data.model.Url;
 import martin.dev.pricer.data.service.StoreService;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Launcher {
 
@@ -30,5 +39,27 @@ public class Launcher {
                         this.storeService.updateUrlStatus(store, url, Status.READY);
                     });
         });
+    }
+
+//    @Scheduled(fixedRate = 600000000, initialDelay = 1)
+    public void insertNewStore() {
+
+        HashMap<String, Set<Category>> data = new HashMap<>();
+
+        data.put("https://www.watcho.co.uk/watches/seiko.html?page=1", Stream.of("Watch").map(Category::new).collect(Collectors.toSet()));
+        data.put("https://www.watcho.co.uk/watches/garmin.html?page=1", Stream.of("Watch").map(Category::new).collect(Collectors.toSet()));
+        data.put("https://www.watcho.co.uk/watches/casio/casio-g-shock-watches.html?page=1", Stream.of("Watch").map(Category::new).collect(Collectors.toSet()));
+        data.put("https://www.watcho.co.uk/watches/citizen.html?page=1", Stream.of("Watch").map(Category::new).collect(Collectors.toSet()));
+
+        Set<Url> urlList = new HashSet<>();
+
+        data.keySet().forEach(urlKey -> urlList.add(new Url(urlKey, LocalDateTime.now(), Status.READY, data.get(urlKey))));
+
+        Store store = new Store("Watcho", "https://www.watcho.co.uk", "https://cdn11.bigcommerce.com/s-f06f69/images/stencil/250x100/final-logo-small_1571639906__21982.original.png", urlList);
+
+        this.storeService.getStoreRepository().save(store);
+
+        System.out.println("finished");
+
     }
 }
