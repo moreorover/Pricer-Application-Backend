5d3757492b6db15d11e618b9c70b8efed14ed6d0
moreorover <mselvenis@gmail.com>
2020-04-08 21:03:53 +0100

    error handling

---
+++ src/test/java/martin/dev/pricer/PricerApplicationTests.java
@@ -0,0 +1,12 @@
+package martin.dev.pricer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricerApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
