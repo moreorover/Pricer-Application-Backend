97855200b936cca7d68364b6c77e9de2fbeb4436
Martin <mselvenis@gmail.com>
2020-04-29 13:05:17 +0100

    sql resource files

---
+++ src/main/resources/db/migration/V1__stores.sql
@@ -0,0 +1,115 @@
+CREATE TABLE `store` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `url` varchar(250) UNIQUE NOT NULL,
+  `logo` varchar(250) UNIQUE NOT NULL
+);
+
+CREATE TABLE `url` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `url` varchar(250) UNIQUE NOT NULL,
+  `checked_at` datetime NOT NULL,
+  `store_id` int NOT NULL,
+  `status_id` int NOT NULL
+);
+
+CREATE TABLE `status` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `satus` varchar(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE `category` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `category` varchar(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE `url_category` (
+  `url_id` int NOT NULL,
+  `category_id` int NOT NULL
+);
+
+ALTER TABLE `url` ADD FOREIGN KEY (`store_id`) REFERENCES `store` (`id`);
+
+ALTER TABLE `url` ADD FOREIGN KEY (`status_id`) REFERENCES `status` (`id`);
+
+ALTER TABLE `url_category` ADD FOREIGN KEY (`url_id`) REFERENCES `url` (`id`);
+
+ALTER TABLE `url_category` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+INSERT INTO `status` (id, satus) VALUES
+(1, 'Ready'),
+(2, 'Processing'),
+(3, 'Disabled');
+
+INSERT INTO `store` (id, name, url, logo) VALUES
+(1, 'Creation Watches', 'https://www.creationwatches.com', 'https://www.complaintsboard.com/img/business/116976/182x300/creation-watches.jpg'),
+(2, 'First Class Watches', 'https://www.firstclasswatches.co.uk', 'https://pbs.twimg.com/profile_images/1080753708953157633/sv90SvHJ.jpg'),
+(3, 'Argos', 'https://www.argos.co.uk', 'https://media.4rgos.it/i/Argos/Argos_Pride_Logo_2?w=120&fmt=png'),
+(4, 'Superdrug', 'https://www.superdrug.com', 'https://pbs.twimg.com/profile_images/1010086590536929280/Dy5TpFig_400x400.jpg'),
+(5, 'H. Samuel', 'https://www.hsamuel.co.uk', 'https://static.cv-library.co.uk/2019/08/29/10-23/hsamuellogo.png'),
+(6, 'Debenhams', 'https://www.debenhams.com', 'https://www.stickpng.com/assets/images/5a1ac8e0f65d84088faf1376.png'),
+(7, 'Ernest Jones', 'https://www.ernestjones.co.uk', 'https://www.marieclaire.co.uk/vouchers/vfiles/33-bec523919c46dd5643c48945ce1c794b.png'),
+(8, 'Watch Shop', 'https://www.watchshop.com', 'http://www.watchshop.com/time-of-your-life/img/watchshop-logo.png'),
+(9, 'AMJ Watches', 'https://amjwatches.co.uk', 'https://s3-eu-west-1.amazonaws.com/tpd/logos/52dfcaca0000640005780190/0x0.png'),
+(10, 'Gold Smiths', 'https://www.goldsmiths.co.uk', 'https://lh3.googleusercontent.com/-Ck01XdjIITQ/AAAAAAAAAAI/AAAAAAAAA9o/iz6zLZRdIZ0/s250-c/photo.jpg'),
+(11, 'Tic Watches', 'https://www.ticwatches.co.uk', 'https://www.ticwatches.co.uk/images/modules/promo_units/1575387494-68252500.png'),
+(12, 'Watcho', 'https://www.watcho.co.uk', 'https://cdn11.bigcommerce.com/s-f06f69/images/stencil/250x100/final-logo-small_1571639906__21982.original.png'),
+(13, 'Simpkins Jewellers', 'https://simpkinsjewellers.co.uk', 'https://simpkinsjewellers.co.uk/image/catalog/Banners/Simpkins%20Logo%202.png');
+
+INSERT INTO `url` (id, url, checked_at, store_id, status_id) VALUES
+(1, 'https://www.creationwatches.com/products/timex-watches-434/index-1-5d.html?currency=GBP', TIMESTAMP '2019-10-24 12:31:00.02315', 1, 3),
+(2, 'https://www.creationwatches.com/products/orient-watches-252/index-1-5d.html?currency=GBP', TIMESTAMP '2019-10-24 12:31:00.02315', 1, 3),
+(3, 'https://www.creationwatches.com/products/tissot-247/index-1-5d.html?currency=GBP', TIMESTAMP '2019-10-24 12:31:00.02315', 1, 3),
+(4, 'https://www.creationwatches.com/products/citizen-74/index-1-5d.html?currency=GBP', TIMESTAMP '2019-10-24 12:31:00.02315', 1, 3),
+(5, 'https://www.creationwatches.com/products/hamilton-watches-250/index-1-5d.html?currency=GBP', TIMESTAMP '2019-10-24 12:31:00.02315', 1, 3),
+(6, 'https://www.creationwatches.com/products/bulova-watches-271/index-1-5d.html?currency=GBP', TIMESTAMP '2019-10-24 12:31:00.02315', 1, 3),
+(7, 'https://www.creationwatches.com/products/seiko-75/index-1-5d.html?currency=GBP', TIMESTAMP '2019-10-24 12:31:00.02315', 1, 3),
+(8, 'https://www.creationwatches.com/products/casio-watches-73/index-1-5d.html?currency=GBP', TIMESTAMP '2019-10-24 12:31:00.02315', 1, 3),
+(9, 'https://www.firstclasswatches.co.uk/mens-watches/?show_stock=y&f50=Mens+Watches&per_page=180&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 2, 3),
+(10, 'https://www.firstclasswatches.co.uk/womens-watches/?show_stock=y&f50=Womens+Watches&per_page=180&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 2, 3),
+(11, 'https://www.argos.co.uk/browse/jewellery-and-watches/watches/ladies-watches/c:29306/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(12, 'https://www.argos.co.uk/browse/health-and-beauty/perfume-and-aftershaves/mens-fragrance/c:29283/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(13, 'https://www.argos.co.uk/browse/technology/smart-technology/smart-speakers/c:658417/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(14, 'https://www.argos.co.uk/browse/jewellery-and-watches/watches/mens-watches/c:29307/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(15, 'https://www.argos.co.uk/browse/technology/smart-technology/smart-home-monitoring/c:30288/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(16, 'https://www.argos.co.uk/browse/technology/smart-technology/smart-heating/c:30286/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(17, 'https://www.argos.co.uk/browse/technology/ipod-mp3-and-headphones/headphones-and-earphones/c:30128/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(18, 'https://www.argos.co.uk/browse/technology/smart-technology/smart-plugs/c:30287/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(19, 'https://www.argos.co.uk/browse/technology/video-games-and-consoles/ps4/ps4-games/c:30037/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(20, 'https://www.argos.co.uk/browse/technology/smart-technology/smart-lighting/c:30285/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(21, 'https://www.argos.co.uk/browse/technology/video-games-and-consoles/nintendo-switch/nintendo-switch-games/c:30292/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(22, 'https://www.argos.co.uk/browse/technology/video-games-and-consoles/xbox-one/xbox-one-games/c:30031/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(23, 'https://www.argos.co.uk/browse/health-and-beauty/perfume-and-aftershaves/ladies-fragrance/c:29282/opt/page:1/', TIMESTAMP '2019-10-24 12:31:00.02315', 3, 3),
+(24, 'https://www.superdrug.com/Fragrance/Perfume-For-Women/c/fragranceforher?q=%3AbestBiz%3AinStockFlag%3Atrue&page=0&resultsForPage=60&sort=bestBiz', TIMESTAMP '2019-10-24 12:31:00.02315', 4, 3),
+(25, 'https://www.superdrug.com/Fragrance/Fragrance-For-Him/c/fragranceforhim?q=%3AbestBiz%3AinStockFlag%3Atrue&page=0&resultsForPage=60&sort=bestBiz', TIMESTAMP '2019-10-24 12:31:00.02315', 4, 3),
+(26, 'https://www.hsamuel.co.uk/webstore/l/watches/recipient%7Cher/?Pg=1', TIMESTAMP '2019-10-24 12:31:00.02315', 5, 3),
+(27, 'https://www.hsamuel.co.uk/webstore/l/watches/recipient%7Chim/?Pg=1', TIMESTAMP '2019-10-24 12:31:00.02315', 5, 3),
+(28, 'https://www.debenhams.com/men/accessories/watches?pn=1&?shipToCntry=GB', TIMESTAMP '2019-10-24 12:31:00.02315', 6, 3),
+(29, 'https://www.debenhams.com/women/accessories/watches?pn=1&?shipToCntry=GB', TIMESTAMP '2019-10-24 12:31:00.02315', 6, 3),
+(30, 'https://www.ernestjones.co.uk/webstore/l/watches/recipient%7Cher/?Pg=1', TIMESTAMP '2019-10-24 12:31:00.02315', 7, 3),
+(31, 'https://www.ernestjones.co.uk/webstore/l/watches/recipient%7Chim/?Pg=1', TIMESTAMP '2019-10-24 12:31:00.02315', 7, 3),
+(32, 'https://www.watchshop.com/ladies-watches.html?show=192&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 8, 3),
+(33, 'https://www.watchshop.com/mens-watches.html?show=192&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 8, 3),
+(34, 'https://amjwatches.co.uk/mens.html?page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 9, 3),
+(35, 'https://amjwatches.co.uk/ladies.html?page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 9, 3),
+(36, 'https://www.goldsmiths.co.uk/c/Watches/Ladies-Watches/filter/Page_1/Psize_96/Show_Page/', TIMESTAMP '2019-10-24 12:31:00.02315', 10, 3),
+(37, 'https://www.goldsmiths.co.uk/c/Watches/Mens-Watches/filter/Page_1/Psize_96/Show_Page/', TIMESTAMP '2019-10-24 12:31:00.02315', 10, 3),
+(38, 'https://www.ticwatches.co.uk/womens-watches-c2', TIMESTAMP '2019-10-24 12:31:00.02315', 11, 3),
+(39, 'https://www.ticwatches.co.uk/mens-watches-c1', TIMESTAMP '2019-10-24 12:31:00.02315', 11, 3),
+(40, 'https://www.watcho.co.uk/watches/citizen.html?page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 12, 3),
+(41, 'https://www.watcho.co.uk/watches/casio/casio-g-shock-watches.html?page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 12, 3),
+(42, 'https://www.watcho.co.uk/watches/garmin.html?page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 12, 3),
+(43, 'https://www.watcho.co.uk/watches/seiko.html?page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 12, 3),
+(44, 'https://simpkinsjewellers.co.uk/watches/seiko?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(45, 'https://simpkinsjewellers.co.uk/watches/citizen?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(46, 'https://simpkinsjewellers.co.uk/watches/casio?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(47, 'https://simpkinsjewellers.co.uk/watches/victorinox?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(48, 'https://simpkinsjewellers.co.uk/watches/police?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(49, 'https://simpkinsjewellers.co.uk/watches/rotary?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(50, 'https://simpkinsjewellers.co.uk/watches/pulsar?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(51, 'https://simpkinsjewellers.co.uk/watches/Mondaine?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(52, 'https://simpkinsjewellers.co.uk/watches/bulova?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(53, 'https://simpkinsjewellers.co.uk/watches/lorus?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(54, 'https://simpkinsjewellers.co.uk/watches/AVI-8%20Aviator%20Watches?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(55, 'https://simpkinsjewellers.co.uk/watches/accurist?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3),
+(56, 'https://simpkinsjewellers.co.uk/watches/bering?limit=100&page=1', TIMESTAMP '2019-10-24 12:31:00.02315', 13, 3);
