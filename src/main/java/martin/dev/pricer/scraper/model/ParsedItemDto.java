eb0b9a513277be31c7b08aa07e59bd287f61dd22
Martin <mselvenis@gmail.com>
2020-05-01 00:01:56 +0100

    ParsedItemDto updated

--- src/main/java/martin/dev/pricer/scraper/model/ParsedItemDto.java
+++ src/main/java/martin/dev/pricer/scraper/model/ParsedItemDto.java
@@ -3,6 +3,9 @@ package martin.dev.pricer.scraper.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import martin.dev.pricer.flyway.model.Url;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,6 +17,17 @@ public class ParsedItemDto {
     private String upc;
     private double price;
     private String urlFound;
+    private LocalDateTime foundTime;
+    private Url urlObject;
+
+    public ParsedItemDto(String title, String url, String img, String upc, double price, String urlFound) {
+        this.title = title;
+        this.url = url;
+        this.img = img;
+        this.upc = upc;
+        this.price = price;
+        this.urlFound = urlFound;
+    }
 
     public boolean isValid() {
         return !this.title.isBlank() && !this.title.isEmpty() &&
