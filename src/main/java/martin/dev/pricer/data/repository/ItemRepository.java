823b2b17e1a62632a73233377c8956a8020e156d
Martin <mselvenis@gmail.com>
2020-05-01 22:12:38 +0100

    ItemRepository return pageable

--- src/main/java/martin/dev/pricer/data/repository/ItemRepository.java
+++ src/main/java/martin/dev/pricer/data/repository/ItemRepository.java
@@ -1,8 +1,11 @@
 package martin.dev.pricer.data.repository;
 
 import martin.dev.pricer.data.model.Item;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends MongoRepository<Item, String> {
@@ -10,4 +13,6 @@ public interface ItemRepository extends MongoRepository<Item, String> {
     Item findByUpc(String s);
 
     Boolean existsByUpc(String upc);
+
+    List<Item> findAllBy(Pageable pageable);
 }
