deee45ccad5f67eb729e3ef4c75d892d92d6fb4b
Martin <mselvenis@gmail.com>
2020-05-01 22:15:24 +0100

    Price remove unused imports

--- src/main/java/martin/dev/pricer/flyway/model/Price.java
+++ src/main/java/martin/dev/pricer/flyway/model/Price.java
@@ -3,7 +3,6 @@ package martin.dev.pricer.flyway.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
