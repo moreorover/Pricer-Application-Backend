96b42891c29d67f40b28ab7a159d96b18cdf6419
Martin <mselvenis@gmail.com>
2020-07-01 09:01:49 +0100

    concurrently running scheduled tasks?

---
+++ src/main/java/martin/dev/pricer/profile/AppConfig.java
@@ -0,0 +1,26 @@
+package martin.dev.pricer.profile;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableScheduling
+public class AppConfig implements SchedulingConfigurer {
+
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean(destroyMethod="shutdown")
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(2);
+    }
+}
