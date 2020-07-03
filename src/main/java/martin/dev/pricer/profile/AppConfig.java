a51b1ef8d449634538bb987027b50f5d309c4b9f
Martin <mselvenis@gmail.com>
2020-07-03 12:12:59 +0100

    clean up

--- src/main/java/martin/dev/pricer/profile/AppConfig.java
+++ src/main/java/martin/dev/pricer/profile/AppConfig.java
@@ -1,23 +1,14 @@
 package martin.dev.pricer.profile;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.regex.Pattern;
 
 @Configuration
 @EnableScheduling
@@ -34,34 +25,4 @@ public class AppConfig implements SchedulingConfigurer {
     public Executor taskExecutor() {
         return Executors.newScheduledThreadPool(2);
     }
-
-//    @Bean
-//    public FilterRegistrationBean spaRedirectFiler() {
-//        FilterRegistrationBean<OncePerRequestFilter> registration = new FilterRegistrationBean<>();
-//        registration.setFilter(createRedirectFilter());
-//        registration.addUrlPatterns("/*");
-//        registration.setName("frontendRedirectFiler");
-//        registration.setOrder(1);
-//        return registration;
-//    }
-//
-//    private OncePerRequestFilter createRedirectFilter() {
-//        return new OncePerRequestFilter() {
-//            // Forwards all routes except '/index.html', '/200.html', '/favicon.ico', '/sw.js' '/api/', '/api/**'
-//            private final String REGEX = "(?!/actuator|/api|/_nuxt|/static|/index\\.html|/200\\.html|/favicon\\.ico|/sw\\.js).*$";
-//            private Pattern pattern = Pattern.compile(REGEX);
-//            @Override
-//            protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
-//                if (pattern.matcher(req.getRequestURI()).matches() && !req.getRequestURI().equals("/")) {
-//                    // Delegate/Forward to `/` if `pattern` matches and it is not `/`
-//                    // Required because of 'mode: history'usage in frontend routing, see README for further details
-//                    log.info("URL {} entered directly into the Browser, redirecting...", req.getRequestURI());
-//                    RequestDispatcher rd = req.getRequestDispatcher("/");
-//                    rd.forward(req, res);
-//                } else {
-//                    chain.doFilter(req, res);
-//                }
-//            }
-//        };
-//    }
 }
