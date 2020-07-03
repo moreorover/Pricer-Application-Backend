efe9c20213bf8f6c2ba1870efe145b622626bc1a
Martin <mselvenis@gmail.com>
2020-07-03 12:16:55 +0100

    home component text

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <h1>Home Component</h1>
   </div>
 </template>
 
@@ -7,7 +8,6 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
-  components: {  }
+  name: 'Home'
 }
 </script>
