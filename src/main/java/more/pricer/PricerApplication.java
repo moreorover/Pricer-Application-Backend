563c585d3cab5d4d304d3e8fdbe67a389acd231e
moreorover <mselvenis@gmail.com>
2022-05-15 19:26:43 +0100

    init

---
+++ src/main/java/more/pricer/PricerApplication.java
@@ -0,0 +1,13 @@
+package more.pricer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricerApplication.class, args);
+	}
+
+}
